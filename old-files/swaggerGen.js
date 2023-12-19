0f6f7d31613c3702ff0cb36ff72de13596869591
Gildemberg Leite <gildembergleite@gmail.com>
2023-12-19 14:24:03 -0300

    lint: remake file and add new rule

--- old-files/swaggerGen.js
+++ old-files/swaggerGen.js
@@ -1,7 +1,7 @@
-function convert() {
-  "use strict";
+export function convert() {
+  'use strict'
   // ---- Global variables ----
-  var inJSON, outSwagger, tabCount, indentator;
+  var inJSON, outSwagger, tabCount, indentator
 
   // ---- Functions definitions ----
   function changeIndentation(count) {
@@ -13,18 +13,18 @@ function convert() {
 	-tabcount
 	*/
 
-    let i;
+    let i
     if (count >= tabCount) {
-      i = tabCount;
+      i = tabCount
     } else {
-      i = 0;
-      indentator = "\n";
+      i = 0
+      indentator = '\n'
     }
     for (; i < count; i++) {
-      indentator += "\t";
+      indentator += '\t'
     }
     //Update tabCount
-    tabCount = count;
+    tabCount = count
   }
 
   function conversorSelection(obj) {
@@ -34,29 +34,29 @@ function convert() {
     -outSwagger
     */
 
-    changeIndentation(tabCount + 1);
-    if (typeof obj === "number") {
+    changeIndentation(tabCount + 1)
+    if (typeof obj === 'number') {
       //attribute is a number
-      convertNumber(obj);
-    } else if (Object.prototype.toString.call(obj) === "[object Array]") {
+      convertNumber(obj)
+    } else if (Object.prototype.toString.call(obj) === '[object Array]') {
       //attribute is an array
-      convertArray(obj);
-    } else if (typeof obj === "object") {
+      convertArray(obj)
+    } else if (typeof obj === 'object') {
       //attribute is an object
-      convertObject(obj);
-    } else if (typeof obj === "string") {
+      convertObject(obj)
+    } else if (typeof obj === 'string') {
       //attribute is a string
-      convertString(obj);
-    } else if (typeof obj === "boolean") {
+      convertString(obj)
+    } else if (typeof obj === 'boolean') {
       // attribute is a boolean
-      outSwagger += indentator + '"type": "boolean"';
+      outSwagger += indentator + '"type": "boolean"'
     } else {
       // not a valid Swagger type
       alert(
         'Property type "' + typeof obj + '" not valid for Swagger definitions'
-      );
+      )
     }
-    changeIndentation(tabCount - 1);
+    changeIndentation(tabCount - 1)
   }
 
   function convertNumber(num) {
@@ -66,21 +66,21 @@ function convert() {
     -outSwagger
     */
 
-    if (num % 1 === 0 && !document.getElementById("noInt").checked) {
-      outSwagger += indentator + '"type": "integer",';
+    if (num % 1 === 0 && !document.getElementById('noInt').checked) {
+      outSwagger += indentator + '"type": "integer",'
       if (num < 2147483647 && num > -2147483647) {
-        outSwagger += indentator + '"format": "int32"';
+        outSwagger += indentator + '"format": "int32"'
       } else if (Number.isSafeInteger(num)) {
-        outSwagger += indentator + '"format": "int64"';
+        outSwagger += indentator + '"format": "int64"'
       } else {
-        outSwagger += indentator + '"format": "unsafe"';
+        outSwagger += indentator + '"format": "unsafe"'
       }
     } else {
-      outSwagger += indentator + '"type": "number"';
+      outSwagger += indentator + '"type": "number"'
     }
-    if (document.getElementById("requestExamples").checked) {
+    if (document.getElementById('requestExamples').checked) {
       //Log example if checkbox is checked
-      outSwagger += "," + indentator + '"example": "' + num + '"';
+      outSwagger += ',' + indentator + '"example": "' + num + '"'
     }
   }
 
@@ -94,19 +94,19 @@ function convert() {
 
     let regxDate = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/,
       regxDateTime =
-        /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]).([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9](\.[0-9]{1,3})?(Z|(\+|\-)([0-1][0-9]|2[0-3]):[0-5][0-9])$/;
+        /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]).([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9](\.[0-9]{1,3})?(Z|(\+|\-)([0-1][0-9]|2[0-3]):[0-5][0-9])$/
 
-    outSwagger += indentator + '"type": "string"';
+    outSwagger += indentator + '"type": "string"'
     if (regxDateTime.test(str)) {
-      outSwagger += ",";
-      outSwagger += indentator + '"format": "date-time"';
+      outSwagger += ','
+      outSwagger += indentator + '"format": "date-time"'
     } else if (regxDate.test(str)) {
-      outSwagger += ",";
-      outSwagger += indentator + '"format": "date"';
+      outSwagger += ','
+      outSwagger += indentator + '"format": "date"'
     }
-    if (document.getElementById("requestExamples").checked) {
+    if (document.getElementById('requestExamples').checked) {
       //Log example if checkbox is checked
-      outSwagger += "," + indentator + '"example": "' + str + '"';
+      outSwagger += ',' + indentator + '"example": "' + str + '"'
     }
   }
 
@@ -116,26 +116,26 @@ function convert() {
     Global variables updated: 
     -outSwagger
     */
-    let schema = {};
-    let examples = new Set();
+    let schema = {}
+    let examples = new Set()
     for (const entry of obj) {
       for (const key of Object.keys(entry)) {
         if (!Object.keys(schema).includes(key)) {
-          examples.add(entry);
-          schema[key] = entry[key];
+          examples.add(entry)
+          schema[key] = entry[key]
         }
       }
     }
 
-    outSwagger += indentator + '"type": "array",';
+    outSwagger += indentator + '"type": "array",'
     // ---- Begin items scope ----
-    outSwagger += indentator + '"items": {';
-    conversorSelection(schema);
-    outSwagger += indentator + "}";
+    outSwagger += indentator + '"items": {'
+    conversorSelection(schema)
+    outSwagger += indentator + '}'
     // ---- End items scope ----
     // ---- Begin example scope ----
-    if (document.getElementById("requestExamples").checked) {
-      outSwagger += ","
+    if (document.getElementById('requestExamples').checked) {
+      outSwagger += ','
       outSwagger += indentator + '"example": ' + JSON.stringify(
         Array.from(examples), null, '\t'
       ).replaceAll('\n', indentator)
@@ -155,32 +155,32 @@ function convert() {
       outSwagger +=
         indentator +
         '"type": "' +
-        document.getElementById("nullType").value +
-        '",';
-      outSwagger += indentator + '"format": "nullable"';
-      return;
+        document.getElementById('nullType').value +
+        '",'
+      outSwagger += indentator + '"format": "nullable"'
+      return
     }
     // ---- Begin properties scope ----
-    outSwagger += indentator + '"type": "object",';
-    outSwagger += indentator + '"properties": {';
-    changeIndentation(tabCount + 1);
+    outSwagger += indentator + '"type": "object",'
+    outSwagger += indentator + '"properties": {'
+    changeIndentation(tabCount + 1)
     //For each attribute inside that object
     for (var prop in obj) {
       // ---- Begin property type scope ----
-      outSwagger += indentator + '"' + prop + '": {';
-      conversorSelection(obj[prop]);
-      outSwagger += indentator + "},";
+      outSwagger += indentator + '"' + prop + '": {'
+      conversorSelection(obj[prop])
+      outSwagger += indentator + '},'
       // ---- End property type scope ----
     }
 
-    changeIndentation(tabCount - 1);
+    changeIndentation(tabCount - 1)
     if (Object.keys(obj).length > 0) {
       //At least 1 property inserted
-      outSwagger = outSwagger.substring(0, outSwagger.length - 1); //Remove last comma
-      outSwagger += indentator + "}";
+      outSwagger = outSwagger.substring(0, outSwagger.length - 1) //Remove last comma
+      outSwagger += indentator + '}'
     } else {
       // No property inserted
-      outSwagger += " }";
+      outSwagger += ' }'
     }
   }
 
@@ -192,46 +192,46 @@ function convert() {
 	*/
     if (yaml) {
       return value
-        .replace(/[{},"]+/g, "")
-        .replace(/\t/g, "  ")
-        .replace(/(^ *\n)/gm, "");
+        .replace(/[{},"]+/g, '')
+        .replace(/\t/g, '  ')
+        .replace(/(^ *\n)/gm, '')
     } else {
-      return value;
+      return value
     }
   }
 
   // ---- Execution begins here ----
-  inJSON = document.getElementById("JSON").value;
+  inJSON = document.getElementById('JSON').value
   try {
-    inJSON = JSON.parse(inJSON);
+    inJSON = JSON.parse(inJSON)
   } catch (e) {
-    alert("Your JSON is invalid!\n(" + e + ")");
-    return;
+    alert('Your JSON is invalid!\n(' + e + ')')
+    return
   }
 
   //For recursive functions to keep track of the tab spacing
-  tabCount = 0;
-  indentator = "\n";
+  tabCount = 0
+  indentator = '\n'
   // ---- Begin definitions ----
-  outSwagger = '"definitions": {';
-  changeIndentation(1);
+  outSwagger = '"definitions": {'
+  changeIndentation(1)
   //For each object inside the JSON
   for (var obj in inJSON) {
     // ---- Begin schema scope ----
-    outSwagger += indentator + '"' + obj + '": {';
-    conversorSelection(inJSON[obj]);
-    outSwagger += indentator + "},";
+    outSwagger += indentator + '"' + obj + '": {'
+    conversorSelection(inJSON[obj])
+    outSwagger += indentator + '},'
     // ---- End schema scope ----
   }
   //Remove last comma
-  outSwagger = outSwagger.substring(0, outSwagger.length - 1);
+  outSwagger = outSwagger.substring(0, outSwagger.length - 1)
   // ---- End definitions ----
-  changeIndentation(tabCount - 1);
-  outSwagger += indentator + "}";
+  changeIndentation(tabCount - 1)
+  outSwagger += indentator + '}'
 
-  document.getElementById("Swagger").value = format(
+  document.getElementById('Swagger').value = format(
     outSwagger,
-    document.getElementById("yamlOut").checked
-  );
+    document.getElementById('yamlOut').checked
+  )
 }
 
